3b2d6879bd82e39a239028ef695ad07909b312b9
Gohar33 <vangogik@gmail.com>
2021-03-19 17:28:51 +0100

    Added asyinc await version of the smae function

--- javascript/javascript3/week2/main.js
+++ javascript/javascript3/week2/main.js
@@ -4,6 +4,8 @@ const greenBox = document.querySelector('ul.marks li:nth-child(3)');
 const boxes = [redBox, blueBox, greenBox];
 const span = document.querySelector('span');
 
+// calling one by one 
+
 function translateOneByOne() {
     new Promise((resolve) => {
         setTimeout(() => {
@@ -30,16 +32,21 @@ function translateOneByOne() {
 }
 translateOneByOne()
 
+//calling one by one - async await version
 
+async function translateOneByOne() {
 
-
-
+    await moveElement(blueBox, { x: 400 - parseInt(blueBox.style.left), y: 300 - parseInt(blueBox.style.top) })
+    console.log("Blue element has been moved");
+    await moveElement(redBox, { x: 20 - parseInt(redBox.style.left), y: 300 - parseInt(redBox.style.top) })
+    console.log("Red element has been moved");
+    moveElement(greenBox, { x: 400 - parseInt(greenBox.style.left), y: 20 - parseInt(greenBox.style.top) })
+    console.log("Green element has been moved");
+}
+translateOneByOne()
 
 
 // calling them at once
-
-
-
 
 async function translateAllAtOnce() {
     try {
