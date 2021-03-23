fcd964563ab570cd45a675e235bbde1dbcd53353
Gohar33 <vangogik@gmail.com>
2021-03-23 19:21:43 +0100

    Implemented feedback

--- javascript/javascript3/week1/giphy.js
+++ javascript/javascript3/week1/giphy.js
@@ -1,4 +1,4 @@
-const input1 = document.querySelector(".search")
+const inputSection = document.querySelector(".search")
 const button = document.querySelector("button")
 const gifCount = document.querySelector(".gif-number")
 document.addEventListener("DomContentLoaded", getGif)
@@ -6,18 +6,18 @@ document.addEventListener("DomContentLoaded", getGif)
 function getGif() {
     button.addEventListener("click", function (content) {
         content.preventDefault();
-        let inputVal = input1.value.trim()
+        const inputVal = inputSection.value.trim()
         let url = `https://api.giphy.com/v1/gifs/search?api_key=TJRmCZYdbW1KDE0BZUYGwuRwksC8RjFu&q=${inputVal}&limit=${gifCount.value}&offset=0&rating=g&lang=en`
         url = url.concat(inputVal)
 
         fetch(url)
             .then(response => response.json())
             .then(gif => {
-                let myDiv = document.getElementById("my-div")
+                const myDiv = document.getElementById("my-div")
                 myDiv.innerHTML = ""
 
                 gif.data.forEach(giphy => {
-                    let imgGif = document.createElement("img")
+                    const imgGif = document.createElement("img")
                     imgGif.src = giphy.images.downsized.url
                     myDiv.appendChild(imgGif)
 
--- javascript/javascript3/week1/weather.js
+++ javascript/javascript3/week1/weather.js
@@ -8,7 +8,7 @@ const weatherResult = document.querySelector(".weather-container")
 
 function getCityWeather() {
     button.addEventListener("click", function () {
-        let inputVal = input.value
+        const inputVal = input.value
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${inputVal}&units=metric&apikey=0284346df0317b81c2e17a35dfbda0c4`
         if (!input.value) {
             alert("You should write a city name")
@@ -20,45 +20,45 @@ function getCityWeather() {
             .then(data => {
                 console.log(data);
 
-                let name = document.querySelector("h2")
+                const name = document.querySelector("h2")
                 name.innerHTML = data.name
                 weatherResult.appendChild(name)
 
                 const icon = "http://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-                let imgIcon = document.querySelector(".img")
+                const imgIcon = document.querySelector(".img")
                 imgIcon.src = icon
                 weatherResult.appendChild(imgIcon)
 
-                let temp = document.querySelector(".temp")
+                const temp = document.querySelector(".temp")
                 temp.innerHTML = Math.floor(data.main.temp) + "°";
                 weatherResult.appendChild(temp)
 
-                let weather = document.querySelector(".weather")
+                const weather = document.querySelector(".weather")
                 weather.innerHTML = data.weather[0].main
                 weatherResult.appendChild(weather)
 
 
 
-                let windSpeed = document.querySelector(".wind")
+                const windSpeed = document.querySelector(".wind")
                 windSpeed.innerHTML = "Wind speed: " + data.wind.speed
                 weatherResult.appendChild(windSpeed)
 
-                let cloudy = document.querySelector(".cloudy")
+                const cloudy = document.querySelector(".cloudy")
                 cloudy.innerHTML = "Cloudy: " + data.weather[0].description
                 weatherResult.appendChild(cloudy)
 
-                let sunrise = document.getElementById("sunrise")
-                let sunrise1 = new Date(data.sys.sunrise * 1000);
-                let timeChange = sunrise1.toLocaleTimeString([], {
+                const sunrise = document.getElementById("sunrise")
+                const sunrise1 = new Date(data.sys.sunrise * 1000);
+                const timeChange = sunrise1.toLocaleTimeString([], {
                     hour: "2-digit",
                     minute: "2-digit"
                 });
                 sunrise.innerHTML = "Sunrise: " + timeChange
                 weatherResult.appendChild(sunrise)
 
-                let sunset = document.querySelector(".sunset")
-                let sunset1 = new Date(data.sys.sunset * 1000);
-                let timeChange2 = sunset1.toLocaleTimeString([], {
+                const sunset = document.querySelector(".sunset")
+                const sunset1 = new Date(data.sys.sunset * 1000);
+                const timeChange2 = sunset1.toLocaleTimeString([], {
                     hour: "2-digit",
                     minute: "2-digit"
                 });
