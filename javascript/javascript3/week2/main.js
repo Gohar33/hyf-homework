85aa728931b7cc6be4d70e8dbced2321247af9a1
Gohar33 <vangogik@gmail.com>
2021-03-27 15:19:34 +0100

    Implemented feedback

--- javascript/javascript3/week2/main.js
+++ javascript/javascript3/week2/main.js
@@ -4,35 +4,8 @@ const greenBox = document.querySelector('ul.marks li:nth-child(3)');
 const boxes = [redBox, blueBox, greenBox];
 const span = document.querySelector('span');
 
-// calling one by one 
 
-function translateOneByOne() {
-    new Promise((resolve) => {
-        setTimeout(() => {
-            moveElement(blueBox, { x: 400 - parseInt(blueBox.style.left), y: 300 - parseInt(blueBox.style.top) }).then(() => {
-                console.log("Blue element has been moved");
-            })
-        }, 1000)
-
-        setTimeout(() => {
-            moveElement(redBox, { x: 20 - parseInt(redBox.style.left), y: 300 - parseInt(redBox.style.top) }).then(() => {
-                console.log("Red element has been moved");
-            })
-        }, 3000)
-
-        setTimeout(() => {
-            moveElement(greenBox, { x: 400 - parseInt(greenBox.style.left), y: 20 - parseInt(greenBox.style.top) }).then(() => {
-                console.log("Green element has been moved");
-            })
-        }, 5000)
-
-        resolve()
-    })
-
-}
-translateOneByOne()
-
-//calling one by one - async await version
+//calling one by one
 
 async function translateOneByOne() {
 
