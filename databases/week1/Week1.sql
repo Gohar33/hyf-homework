e4853dbd44388023349e47a7d77463f91ff654d6
Gohar33 <vangogik@gmail.com>
2021-04-03 10:26:38 +0200

    Feedback implemented

--- databases/week1/Week1.sql
+++ databases/week1/Week1.sql
@@ -1,60 +1,57 @@
--- 1. Find out how many tasks are in the task table*/
+-- 1. Find out how many tasks are in the task table
 SELECT COUNT(*)
 FROM task;
--- 35
+
 
 -- 2. Find out how many tasks in the task table do not have a valid due date
 SELECT COUNT(*)
 FROM task
 WHERE due_date IS null;
--- 8
 
 -- 3. Find all the tasks that are marked as done
-SELECT *
+SELECT task.id,task.title,task.description, status.name
 FROM task
 INNER JOIN status ON task.status_id = status.id
 WHERE name LIKE "%Done";
--- found all done
+
 
 -- 4. Find all the tasks that are not marked as done
-SELECT *
+SELECT task.id,task.title,task.description, status.name
 FROM task
 INNER JOIN status ON task.status_id = status.id
 WHERE name NOT LIKE "%Done";
--- found not started or in progress
+
 
 -- 5. Get all the tasks, sorted with the most recently created first
-SELECT *
+SELECT task.id,task.title,task.description, task.created, task.updated
 FROM task
 ORDER BY created DESC;
--- starts with 25 Look at apartments in Ørestad created on 2017-10-30
+
 
 -- 6. Get the single most recently created task
-SELECT *
+SELECT task.id,task.title,task.description, task.created, task.updated
 FROM task
 ORDER BY created DESC
 LIMIT 1;
--- only one row with 25 Look at apartments in Ørestad
 
 -- 7. Get the title and due date of all tasks where the title or description contains database
-SELECT task.title, task.due_date, status.name
+SELECT task.title, task.due_date
 FROM task
-INNER JOIN status ON task.status_id = status_id
 WHERE title LIKE '%database%' OR description LIKE '%database%';
--- got 5 rows
+
 
 -- 8 Get the title and status (as text) of all tasks
 SELECT task.title, status.name AS text
 FROM task
 INNER JOIN status ON task.status_id=status.id;
--- got title and text status
+
 
 -- 9. Get the name of each status, along with a count of how many tasks have that status
 SELECT status.name, COUNT(*)
 FROM task
 INNER JOIN status ON task.status_id=status.id
 GROUP BY status.name;
--- 3 status names with counts
+
 
 
 -- 10. Get the names of all statuses, sorted by the status with most tasks first
@@ -63,5 +60,5 @@ FROM task
 INNER JOIN status ON task.status_id = status.id
 GROUP BY status.name
 ORDER BY COUNT(*)DESC;
--- upper row starts with count 15
+
 
